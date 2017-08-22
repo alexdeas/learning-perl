c3d379d66e13f1661127fbad04d8ef199d2043b9
jhhmastercard <john.ho@mastercard.com>
2017-08-22 14:58:53 +0100

    Typo

--- chapter_02/jhh/exercise2.pl
+++ chapter_02/jhh/exercise2.pl
@@ -4,4 +4,4 @@ print "Enter the radius, then enter: ";
 chomp( $radius = <STDIN> );
 $pi = 3.141592654;
 
-print "The circumferance of a circle with $radius is " . ( 2 * $pi * $radius ) . "\n";
+print "The circumference of a circle with $radius is " . ( 2 * $pi * $radius ) . "\n";
