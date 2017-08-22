b9f204b5dad2d30c483957cb3bcd8ac566d02f5a
jhhmastercard <john.ho@mastercard.com>
2017-08-22 14:58:29 +0100

    Typo

--- chapter_02/jhh/exercise1.pl
+++ chapter_02/jhh/exercise1.pl
@@ -3,4 +3,4 @@
 $pi = 3.141592654;
 $radius = 12.5;
 
-print "The circumferance of a circle with $radius is " . ( 2 * $pi * $radius ) . "\n";
+print "The circumference of a circle with $radius is " . ( 2 * $pi * $radius ) . "\n";
