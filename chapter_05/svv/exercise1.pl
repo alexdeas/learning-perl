b16c2b92d2f7e73d594aff5d26eda8eecc2100a0
svalchinov <svalchinov>
2017-09-11 14:41:28 +0100

    Stefan: chapter 5

---
+++ chapter_05/svv/exercise1.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use autodie;
+
+print reverse(<>);
+
+
+
+
+# $ perl exercise1.pl files/fred files/barney files/betty
+# betty line 3
+# betty line 2
+# betty line 1
+# barney line 3
+# barney line 2
+# barney line 1
+# fred line 3
+# fred line 2
+# fred line 1
